6b2788eceb46cec08602c3bbe07aa9f5850e9f98
Michael Tiller <michael.tiller@gmail.com>
2017-05-23 19:03:44 -0400

    Basic app

---
+++ _table/src/app.tsx
@@ -0,0 +1,7 @@
+import React = require('react');
+
+export class App extends React.Component<{}, void> {
+    render() {
+        return <span>Hello</span>;
+    }
+}
---
+++ _table/src/index.tsx
@@ -0,0 +1,6 @@
+import React = require('react');
+import ReactDOM = require('react-dom');
+
+import { App } from './app';
+
+ReactDOM.render(<App />, document.getElementById("tool-table"));
